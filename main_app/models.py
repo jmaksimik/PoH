4749be5daeceaf00eb8b1bce8923aafe6c7a1adb
jmaksimik <jmaks@me.com>
2023-01-05 15:15:36 -0500

    Add main models

--- main_app/models.py
+++ main_app/models.py
@@ -3,14 +3,35 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Doctor(models.Model): 
+    SPECIALITIES = [ ('FM', 'Family Medicine'),
+                 ('OR', 'Orthopedics'),
+                 ('IM', 'Internal Medicine'),
+               ]
+
+    name = models.CharField(max_length=50)
+    speciality = models.CharField(
+        choices = SPECIALITIES,
+        max_length = 5
+    )
+
+    def __str__(self): 
+        return self.name
+
 class Patient(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthdate = models.DateField(verbose_name = 'Birthdate')
     sex = models.CharField(
         verbose_name = 'Sex Assigned at Birth',
-        choices = ['M', 'F'],
+        choices = [('M', 'Male'), ('F', 'Female')],
+        max_length = 1,
     )
+    doctors = models.ManyToManyField(Doctor)
+
 
 class Appointments(models.Model): 
     date = models.DateField(verbose_name='Appointment Date')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
