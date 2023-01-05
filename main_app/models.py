33a628841cd491ef4319e8913631d94715cd8be1
jmaksimik <jmaks@me.com>
2023-01-05 14:56:11 -0500

    Define Patient model extension of User

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Patient(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField(verbose_name = 'Birthdate')
+    sex = models.CharField(
+        verbose_name = 'Sex Assigned at Birth',
+        choices = ['M', 'F'],
+    )
+
+class Appointments(models.Model): 
+    date = models.DateField(verbose_name='Appointment Date')
+
