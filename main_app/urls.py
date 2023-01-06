0c103d7d1e40da57ccddae4510339d76c37b5a4c
MLeys <102929892+MLeys@users.noreply.github.com>
2023-01-05 22:26:24 -0500

    Added Appointment Create/ Detail/ Update/ and Delete functionality with basic html/css

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('dashboard/', views.dash_index, name='index'),
     path('dashboard/profile', views.update_profile, name='profile_update'),
     path('appointments/', views.appointments_index, name='appointments_index'),
+    path('appointments/<int:pk>/', views.AppointmentDetail.as_view(), name='appointments_detail'),
+    path('appointments/create/', views.AppointmentCreate.as_view(), name='appointments_create'),
+	path('appointments/<int:pk>/update/', views.AppointmentUpdate.as_view(), name='appointments_update'),
+	path('appointments/<int:pk>/delete/', views.AppointmentDelete.as_view(), name='appointments_delete'),
     path('accounts/signup', views.signup, name='signup'),
+
 ]
