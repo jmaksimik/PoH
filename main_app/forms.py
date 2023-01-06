f06525814fbef27970cc2734a4612d87242c5405
jmaksimik <jmaks@me.com>
2023-01-06 16:12:04 -0500

    Fix bug in appt CreateView

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,12 +3,12 @@ from .models import Patient
 from django.contrib.auth.models import User
 from django import forms
 
-class UserForm(forms.ModelForm):
+class UserForm(ModelForm):
     class Meta: 
         model = User
         fields = ['first_name', 'last_name', 'email']
 
-class PatientForm(forms.ModelForm):
+class PatientForm(ModelForm):
     class Meta: 
         model = Patient 
         fields = ['birthdate', 'sex', 'doctors']
