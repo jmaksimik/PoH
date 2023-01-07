d8d581d5894f8245d749b4a688acf2d698e2bbc5
MLeys <102929892+MLeys@users.noreply.github.com>
2023-01-07 17:11:08 -0500

    Customizing form fields for Prescription - Successfully incorperated med autofil from API. FIXME: Strength form not autopopulating choices yet, but is functioning

--- main_app/forms.py
+++ main_app/forms.py
@@ -14,6 +14,42 @@ class PatientForm(ModelForm):
         fields = ['birthdate', 'sex', 'doctors']
 
 class PrescriptionForm(ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    name = forms.CharField(widget=forms.TextInput
+        (attrs={
+            "class": "form-control ",
+            "id": "prescriptionName",
+            "placeholder": "Medication Name"
+        }))
+    size = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class": "form-control ",
+            "id": "prescriptionStrength",
+            "placeholder": "Available Doses"
+        }
+    ))
+    instructions = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "rows": 3
+        }
+    ))
     class Meta:
         model = Prescription
         fields = ['name', 'size', 'doctor', 'instructions', 'notes', 'prescribed']
+
+    # def __int__(self, *args, **kwargs):
+    #     super().__init__(args, **kwargs)
+    #     for field in self.fields:
+    #         print(field)
+    #         new_data = {
+    #             "placeholder": f'Recipe {str(field)}',
+    #             "class": "form-control",
+                
+    #         }
+
+    #         self.fields[str(field)].widget.attrs.update(
+    #             new_data
+    #         )
+    #     self.fields['name'].label = "NAME HERE"
+        # self.fields['name'].widget.attrs.update({'class': "form-control-2"})
